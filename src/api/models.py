b0621d9d524b163321ba7855d5038e44027047be
Angel Gallego <angelgb1990@gmail.com>
2024-12-15 23:04:26 +0000

    creados puntos apis

--- src/api/models.py
+++ src/api/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),  nullable=False)
+    user_name = db.Column(db.String(50), unique=True,  nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -15,7 +15,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
+            "user_name": self.user_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -23,22 +22,49 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@api.route('register', methods=['POST'])
+def handle_register():
+    data = request.json
+    user_name = data.get('user_name')
+    email = data.get('email')
+    password = data.get('password')
+    is_active = data.get('is_active', True)
+
+    if not user_name or not email or not password:
+        return jsonify({"msg" : "Por favor rellene todos los campos"}), 400
+    
+    if "@" not in email or "." not in email:
+        return jsonify({"msg": "Por favor introduzca un correo válido"}), 400
+    
+    
+    
+    user_existente = User.query.filter((User.user_name == user_name) | (User.email == email)).first()
+    if user_existente:
+        return jsonify({"msg": "El correo ya está registrado"}), 400
+    
+    new_user = User(user_name=user_name, email=email, password=password, is_active=is_active)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"ok" : user_name}), 200
+
 @api.route('/login', methods=['POST'])
 def handle_login():
     data = request.json
-    email = data.get('email')
+    user_name = data.get('user_name')
     password = data.get('password')
-    if not email or not password:
-        return jsonify({"msg" : "El email y el password son requeridos"}), 400
+    if not user_name or not password:
+        return jsonify({"msg" : "El usuario y el password son requeridos"}), 400
     
-    user = User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(user_name=user_name, password=password).first()
     if not user:
         return jsonify({"msg" : "no existe ese usuario"})
     
-    token = create_access_token(identity=user.email)
+    token = create_access_token(identity=user.user_name)
     return jsonify({"token" : token })
 
-# @api.route('/private', methods=['POST'])
-# @jwt_required()
-# def private():
-#     data = request.json
+@api.route('/private', methods=['POST'])
+@jwt_required()
+def private():
+    current_user = get_jwt_identity()
+    return jsonify({"ok" : True , "current_user" : current_user}), 200
